82f5d9e4059279293931c0208adb48a3ab264aed
Minh Vu <minhvu@Minhs-MacBook-Pro.local>
2025-02-12 08:38:28 -0800

    add PlaerDetails.jsx

---
+++ src/PlayerDetails.jsx
@@ -0,0 +1,28 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { fetchPlayerById } from "./puppy-bowl.jsx";
+
+function PlayerPage() {
+  const { playerId } = useParams();
+  const [player, setPlayer] = useState(null);
+
+  useEffect(() => {
+    const getPlayer = async () => {
+      const data = await fetchPlayerById(playerId);
+      setPlayer(data);
+    };
+    getPlayer();
+  }, [playerId]);
+
+  if (!player) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>{player.name}</h1>
+      <p>Owner: {player.owner}</p>
+      <p>Team: {player.team}</p>
+    </div>
+  );
+}
+
+export default PlayerPage;
